8b23b1c18dbc1bcf6fe7f044531411503e1a4bc0
Danya <danilomaster2003@ukr.net>
2022-05-07 18:24:24 +0300

    feat: add style for keyboard

---
+++ virtual-keyboard/src/index.js
@@ -0,0 +1,4 @@
+//import "./assets/style.css"
+import "./assets/style.scss"
+import {first_row} from "./keyboard/main.js"
+
---
+++ virtual-keyboard/src/keyboard/key.js
@@ -0,0 +1,8 @@
+export class Key{
+    constructor(class_name, static_text, en, ru){
+        this.class_name = class_name;
+        this.static_text = static_text;
+        this.en = en;
+        this.ru = ru;
+    }
+}
